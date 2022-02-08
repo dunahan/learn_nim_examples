00bd369b83b04621615d313c3597da1d1fd0fb71
dunahan <25346708+dunahan@users.noreply.github.com>
2022-02-08 08:28:21 +0100

    Create helloworld.nim

---
+++ basic/helloworld.nim
@@ -0,0 +1,6 @@
+# this is a one line comment ;-)
+#[ first step, test installation or the simple Hello World,
+as practiced in every programming language. all within a
+multiline comment ;-) ]#
+
+echo "Hello World!"
