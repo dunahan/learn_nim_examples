d7dd94a8354e8368a5f50fb5d79af89ecf8f8cf2
dunahan <25346708+dunahan@users.noreply.github.com>
2022-02-08 10:53:02 +0100

    Create characters.nim

---
+++ basic/characters.nim
@@ -0,0 +1,15 @@
+#[ a variable with char type would represent a single ASCII char.
+this can be letters, symbols or single numbers.
+it would be declared like 'z'. however, it's single so 'ab' would
+throw a error.]#
+
+let
+  h = 'z'
+  i = '+'
+  j = '2'
+# k = '35'  # error
+# l = 'xy'  # error
+
+echo h
+echo i
+echo j
