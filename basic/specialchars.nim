11ae0ad9bb592f94dc34a866a0d3ccfc323ebc75
dunahan <25346708+dunahan@users.noreply.github.com>
2022-02-08 13:22:44 +0100

    Create specialchars.nim

---
+++ basic/specialchars.nim
@@ -0,0 +1,6 @@
+#[ there exist some specials like \n, \t and \\. those work
+like <\n>ewline, <\t>ab character and <\\>backslash. to write
+those really, use <\\> or <r> rigth after echo ]#
+
+echo "some\nim\tips"
+echo r"some\nim\tips"
