b20f211a4192d600f7b7bec0021c19af0465ba11
dunahan <25346708+dunahan@users.noreply.github.com>
2022-02-08 13:16:48 +0100

    Create strings.nim

---
+++ basic/strings.nim
@@ -0,0 +1,15 @@
+#[ strings can be a series of characters and is written in
+double quotes <"> ]#
+
+let
+  m = "word"
+  n = "A sentence with interpunction."
+  o = ""
+  p = "32"
+  q = "!"
+
+echo m
+echo n
+echo o
+echo p
+echo q
