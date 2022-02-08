e4f95f56e9d847165eccc3ce674071a774f40be4
dunahan <25346708+dunahan@users.noreply.github.com>
2022-02-08 09:37:48 +0100

    Create floats.nim

---
+++ basic/floats.nim
@@ -0,0 +1,18 @@
+#[ floats for example 2.73, -3.14, 5.0 and 4e7 (which means
+4 * 10^7). you can use the four math operations '+ - * /' but
+div and mod aren't defined.
+
+also the normal math precedence operations is as one would expect.
+then it would be 'point before line' (some german saying i know).
+echo 2 + 3 * 4 would be 14.
+
+some sidenotes here: tab's are not supported, so use spaces!]#
+
+let
+  c = 6.75
+  d = 2.25
+
+echo "c + d = ", c + d  # = 9.0
+echo "c - d = ", c - d  # = 4.5
+echo "c * d = ", c * d  # = 15.1875
+echo "c / d = ", c / d  # = 3.0
