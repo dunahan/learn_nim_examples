e887c5c03993778edf31ad5e5311c61e30b9392a
Tobias Wirth <25346708+dunahan@users.noreply.github.com>
2023-07-20 15:08:49 +0200

    Update 13_loop_exercises.nim

--- basic/13_loop_exercises.nim
+++ basic/13_loop_exercises.nim
@@ -3,4 +3,38 @@ First pick a number, if odd multiply by three
 and add one. If even, divide it by two. Repeat.
 Finally ending at the number one ]#
 
+var
+  i = 5
+  
+while i != 1:
+  if i mod 2 == 0:
+    i = (i/2).toInt
+    echo i
+  else:
+    i = 3*i + 1
+    echo i    
+
+
+#[ Iterate through your name:
+Use for-loop to scan your name and print
+only vowels (a e i o u) ]#
+
+let name = "Tobias"
+  
+for letter in name:
+  case letter
+  of 'a':
+    echo letter
+  of 'e':
+    echo letter
+  of 'i':
+    echo letter
+  of 'o':
+    echo letter
+  of 'u':
+    echo letter
+  else: discard
+
+
+#[Fizz buzz]#
 # tbc
