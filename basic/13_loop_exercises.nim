a15cd6565a989c9d33272ececce1ef9b0ae3a84f
Tobias Wirth <25346708+dunahan@users.noreply.github.com>
2023-07-21 08:42:54 +0200

    Update 13_loop_exercises.nim

--- basic/13_loop_exercises.nim
+++ basic/13_loop_exercises.nim
@@ -37,13 +37,26 @@ for letter in name:
 
 
 #[Fizz buzz]#
-#[ Fizz buzz 
-But 5 & 3 missing yet ]#
+#[ Fizz buzz:
+We count numbers from one upwards. If a number is
+divisible by 3 replace it with fizz, if it is
+divisible by 5 replace it with buzz, and if a
+number is divisible by 15 (both 3 and 5) replace
+it with fizzbuzz ]#
 
 for n in countup(1, 30, 1):
-  if n mod 3 == 0:
+  if n mod 3 == 0 and n mod 5 == 0:
+      echo "fizzbuzz"  
+  elif n mod 3 == 0:
     echo "fizz"
   elif n mod 5 == 0:
     echo "buzz"
   else:
     echo n
+
+
+#[ conversion table in to cm:
+where 1 in = 2.54 cm ]#
+
+echo "in  | cm \n----------------"
+# hint countup(1, 16, 3), tbc
